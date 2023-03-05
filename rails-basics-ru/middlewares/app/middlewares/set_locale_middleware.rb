8a7c80bf961e78387fc01ddb884c21e335f5ab45
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-03-05 14:33:50 +0100

    add checking of unavailable locales

--- rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -11,14 +11,15 @@ class SetLocaleMiddleware
   end
 
   def _call(env)
-    I18n.locale = extract_locale_from_accept_language_header(Rack::Request.new(env)) || I18n.default_locale
+    I18n.locale = extract_locale_from_accept_language_header(Rack::Request.new(env))
     @app.call(env)
   end
 
   private
 
   def extract_locale_from_accept_language_header(request)
-    (request.env['HTTP_ACCEPT_LANGUAGE'] || '').scan(/^[a-z]{2}/).first
+    locale = (request.env['HTTP_ACCEPT_LANGUAGE'] || '').scan(/^[a-z]{2}/).first
+    I18n.locale_available?(locale) ? locale : I18n.default_locale
   end
   # END
 end
