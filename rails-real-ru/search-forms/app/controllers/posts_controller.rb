4d00bd9e3e7c53db14f062215cc302bc0a29019f
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-03-11 02:41:27 +0100

    add homework with decision for Search forms

---
+++ rails-real-ru/search-forms/app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  def index
+    # BEGIN
+    @query = Post.ransack(params[:q])
+    @posts = @query.result
+    @post_states = Post.aasm.states.map { |state| [state.default_display_name, state.name] }
+    # END
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find params[:id]
+
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find params[:id]
+
+    @post.destroy
+
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body, :aasm_state)
+  end
+end
