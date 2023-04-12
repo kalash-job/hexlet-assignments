63a082d49a0f497f8c586023f4f8dd99370f1efd
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-04-12 17:53:21 +0200

    add rails api homework decision

--- rails-full-ru/api/app/controllers/api/users_controller.rb
+++ rails-full-ru/api/app/controllers/api/users_controller.rb
@@ -2,6 +2,12 @@
 
 class Api::UsersController < Api::ApplicationController
   # BEGIN
-  
+  def index
+    respond_with User.all
+  end
+
+  def show
+    respond_with User.find(params[:id])
+  end
   # END
 end
